8703c0beff7595c123e0d5c57f4eab92494e1f13
Nate-Baker <nbaker10987654321@gmail.com>
2024-12-29 19:44:10 -0600

    Dec 29 2024

--- flake.nix
+++ flake.nix
@@ -32,6 +32,7 @@
           ./modules/gnome.nix
           ./modules/nix-settings.nix
           ./modules/wine.nix
+          ./modules/hardware-configuration.nix
 
           # Integrate Home Manager
           home-manager.nixosModules.home-manager
