4de6e6d676fe86b7863b41a0d83a9d9f2618efa6
Nate-Baker <nbaker10987654321@gmail.com>
2024-12-29 19:36:58 -0600

    Dec 29 2024

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader = {
-    grub2 = {
-      enable = true;
-      version = 2;  # Explicitly specify GRUB2 version
-      devices = [ "/dev/nvme0n1" ];  # Use your boot device
-      efiSupport = true;             # Enable for UEFI systems
-      efiInstallAsRemovable = false; # Set to true for some dual-boot scenarios
-      extraConfig = ''
-        # Add any GRUB2-specific configurations here
-      '';
-    };
-    systemd-boot.enable = false;  # Disable other boot loaders if enabled
-    grub.enable = false;          # Ensure the older GRUB is disabled
+  boot.loader.grub = {
+    enable = true;
+    version = 2;  # Specify GRUB2 explicitly
+    devices = [ "/dev/nvme0n1" ];  # Use your boot device
+    efiSupport = true;             # Enable for UEFI systems
+    efiInstallAsRemovable = false; # Set to true for some dual-boot scenarios
+    extraConfig = ''
+      # Add any GRUB2-specific configurations here
+    '';
   };
-}
+
+  # Ensure other bootloaders are disabled
+  boot.loader.systemd-boot.enable = false;
+};
+
 
 
