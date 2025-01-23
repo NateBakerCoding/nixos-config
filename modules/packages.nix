418b6ec1e15aed66601c71ad3973d4a1d9846a47
Nate-Baker <nbaker10987654321@gmail.com>
2025-01-23 00:54:02 -0600

    Jan 23 2025 - adding in opentabletdriver for drawing tablet

--- modules/packages.nix
+++ modules/packages.nix
@@ -43,6 +43,9 @@
   # Allow insecure packaging
   nixpkgs.config.permittedInsecurePackages = [
     "dotnet-runtime-wrapped-6.0.36"
+    "dotnet-runtime-6.0.36"
+    "dotnet-sdk-wrapped-6.0.428"
+    "dotnet-sdk-6.0.428"
   ];
 
 }
